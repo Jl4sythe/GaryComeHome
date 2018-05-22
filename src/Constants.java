f518549eaae2ccfef7f1015ce9e182f8e243dd54
Sash132 <sashg08@gmail.com>
2018-05-22 11:16:08 -0700

    ...

--- src/Constants.java
+++ src/Constants.java
@@ -1,12 +1,12 @@
 import com.badlogic.gdx.math.Vector2;
 
 public class Constants {
-    public final static float WORLD_WIDTH = 1920f;
-    public final static float WORLD_HEIGHT = 1080f;
-    public final static float GRAVITY = -900f;
-    public final static float GARY_SPEED = 533f;
-    public final static int GARY_WIDTH = 266;
-    public final static int GARY_HEIGHT = 224;
+    public final static float WORLD_WIDTH = 960f;
+    public final static float WORLD_HEIGHT = 540f;
+    public final static float GRAVITY = -981f;
+    public final static float GARY_SPEED = 266.5f;
+    public final static int GARY_WIDTH = 133;
+    public final static int GARY_HEIGHT = 112;
     public final static float FLOOR = 100;
-    public final static int SHELL_SIZE = 50;
+    public final static int SHELL_SIZE = 25;
 }
