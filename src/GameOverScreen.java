b3e120802401125a95d618cc82c275bbdae766de
legofatman <jl4sythe00@gmail.com>
2018-05-22 11:04:20 -0700

    .

--- src/GameOverScreen.java
+++ src/GameOverScreen.java
@@ -1,9 +1,30 @@
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameOverScreen implements Screen{
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private BitmapFont font;
+    private Texture background;
 
-    public GameOverScreen(){
+    private GaryGame game;
 
+    public GameOverScreen(GaryGame game){
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(Constants.WORLD_WIDTH,Constants.WORLD_HEIGHT,camera);
+        batch = new SpriteBatch();
+        font = new BitmapFont(Gdx.files.internal("assets/KarbyParty.fnt"));
+        //background = new Texture(Gdx.files.internal("assets/"));
+
+        this.game = game;
     }
 
     @Override
@@ -13,12 +34,19 @@ public class GameOverScreen implements Screen{
 
     @Override
     public void render(float v) {
+        Gdx.gl.glClearColor(0,0,.2f,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        //batch.draw(background,0f,0f,Constants.WORLD_WIDTH,Constants.WORLD_HEIGHT);
+
+        batch.end();
     }
 
     @Override
-    public void resize(int i, int i1) {
-
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
     }
 
     @Override
@@ -38,6 +66,7 @@ public class GameOverScreen implements Screen{
 
     @Override
     public void dispose() {
-
+        batch.dispose();
+        font.dispose();
     }
 }
