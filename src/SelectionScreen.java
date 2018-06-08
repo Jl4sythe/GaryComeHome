be1ad10d32b30e8b7437324a53fb77d17dcaeecd
sash132 <sashg08@gmail.com>
2018-06-07 21:11:29 -0700

    ...

--- src/SelectionScreen.java
+++ src/SelectionScreen.java
@@ -22,6 +22,7 @@ public class SelectionScreen implements Screen
     private Texture menuBackground;
     private ShapeRenderer renderer;
     private Texture button;
+    private Texture button2;
     private Texture word;
     private Vector2 pos;
 
@@ -40,6 +41,7 @@ public class SelectionScreen implements Screen
         font = new BitmapFont(Gdx.files.internal("assets/KarbyParty.fnt"));
         renderer = new ShapeRenderer();
         button = new Texture(Gdx.files.internal("assets/Button.png"));
+        button2 = new Texture(Gdx.files.internal("assets/ButtonState2.png"));
         word = new Texture(Gdx.files.internal("assets/play word.png"));
         menuBackground = new Texture(Gdx.files.internal("assets/menuBackground.jpg"));
         pos = new Vector2(0,0);
@@ -70,6 +72,64 @@ public class SelectionScreen implements Screen
         batch.draw(button,Constants.WORLD_WIDTH - 630,Constants.WORLD_HEIGHT - 510,300,100);
         batch.draw(button,Constants.WORLD_WIDTH - 315,Constants.WORLD_HEIGHT - 510,300,100);
 
+
+        int x = Gdx.input.getX();
+        int y = Gdx.input.getY();
+        pos = viewport.unproject(new Vector2(x, y));
+
+        if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
+            batch.draw(button2,Constants.WORLD_WIDTH - 945,Constants.WORLD_HEIGHT - 190,300,100);
+        else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
+            batch.draw(button2,Constants.WORLD_WIDTH - 630,Constants.WORLD_HEIGHT - 190,300,100);
+        else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
+            batch.draw(button2,Constants.WORLD_WIDTH - 315,Constants.WORLD_HEIGHT - 190,300,100);
+        else if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
+            batch.draw(button2,Constants.WORLD_WIDTH - 945,Constants.WORLD_HEIGHT - 350,300,100);
+        else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
+            batch.draw(button2,Constants.WORLD_WIDTH - 630,Constants.WORLD_HEIGHT - 350,300,100);
+        else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
+            batch.draw(button2,Constants.WORLD_WIDTH - 315,Constants.WORLD_HEIGHT - 350,300,100);
+        else if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
+            batch.draw(button2,Constants.WORLD_WIDTH - 945,Constants.WORLD_HEIGHT - 510,300,100);
+        else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
+            batch.draw(button2,Constants.WORLD_WIDTH - 630,Constants.WORLD_HEIGHT - 510,300,100);
+        else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
+            batch.draw(button2,Constants.WORLD_WIDTH - 315,Constants.WORLD_HEIGHT - 510,300,100);
+
+        batch.end();
+
+
+        if(Gdx.input.justTouched())
+        {
+            x = Gdx.input.getX();
+            y = Gdx.input.getY();
+
+            pos = viewport.unproject(new Vector2(x,y));
+            if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
+                game.setScreen(new LevelOneScreen(game));
+            else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
+                game.setScreen(new LevelTwoScreen(game));
+            else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
+                game.setScreen(new LevelThreeScreen(game));
+            else if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
+                game.setScreen(new LevelFourScreen(game));
+            else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
+                game.setScreen(new LevelFiveScreen(game));
+            else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
+                game.setScreen(new LevelSixScreen(game));
+            else if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
+                game.setScreen(new LevelSevenScreen(game));
+            else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
+                game.setScreen(new LevelEightScreen(game));
+            else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
+                game.setScreen(new MinigameScreen(game));
+
+            GaryCharacter.time = Constants.POWER_TIME;
+            EnemyCharacter.time = Constants.POWER_TIME;
+        }
+
+        batch.begin();
+
         GlyphLayout buttonOneLayout = new GlyphLayout(font, "PINEAPPLE");
         font.draw(batch, "PINEAPPLE", Constants.WORLD_WIDTH - buttonOneLayout.width/2 - 795,Constants.WORLD_HEIGHT + buttonOneLayout.height/2 - 140);
         GlyphLayout buttonTwoLayout = new GlyphLayout(font, "KRUSTY KRAB");
@@ -92,36 +152,6 @@ public class SelectionScreen implements Screen
         font.draw(batch, "MINIGAME", Constants.WORLD_WIDTH - buttonNineLayout.width/2 - 165,Constants.WORLD_HEIGHT + buttonNineLayout.height/2 - 460);
 
         batch.end();
-
-        if(Gdx.input.justTouched())
-        {
-            int x = Gdx.input.getX();
-            int y = Gdx.input.getY();
-
-            pos = viewport.unproject(new Vector2(x,y));
-            if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
-                game.setScreen(new LevelOneScreen(game));
-            else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
-                game.setScreen(new LevelTwoScreen(game));
-            else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 190 && pos.y <= Constants.WORLD_HEIGHT - 90)
-                game.setScreen(new LevelThreeScreen(game));
-            if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
-                game.setScreen(new LevelFourScreen(game));
-            else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
-                game.setScreen(new LevelFiveScreen(game));
-            else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 350 && pos.y <= Constants.WORLD_HEIGHT - 250)
-                game.setScreen(new LevelSixScreen(game));
-            if(pos.x >= Constants.WORLD_WIDTH - 945 && pos.x < Constants.WORLD_WIDTH - 645 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
-                game.setScreen(new LevelSevenScreen(game));
-            else if(pos.x >= Constants.WORLD_WIDTH - 630 && pos.x < Constants.WORLD_WIDTH - 330 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
-                game.setScreen(new LevelEightScreen(game));
-            else if(pos.x >= Constants.WORLD_WIDTH - 315 && pos.x < Constants.WORLD_WIDTH - 15 && pos.y >= Constants.WORLD_HEIGHT - 510 && pos.y <= Constants.WORLD_HEIGHT - 410)
-                game.setScreen(new MinigameScreen(game));
-
-            GaryCharacter.time = Constants.POWER_TIME;
-            EnemyCharacter.time = Constants.POWER_TIME;
-
-        }
     }
 
     public void resize(int width, int height)
